8c13b3142129ad2dd456b128bf62e88895758dcf
Neville Oronni <nevooronni@gmail.com>
2018-03-23 23:10:57 +0300

    faq component and cosume jsonplaceholder api

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-faq',
@@ -7,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+	//create property
+	faqs: Array<any>;
 
+	//pass http instance
+  constructor(private http:Http) { 
+
+  	//make request
+  	this.http.get("https://jsonplaceholder.typicode.com/posts")
+  		//get the reponse in json format
+  		.map(response => response.json())
+  		//add responses the faqs array property
+  		.subscribe(res => this.faqs = res);
+  }
+
+  //logic for when component initializes
   ngOnInit() {
   }
 
