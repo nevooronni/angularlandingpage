32600641c71f07ff7a557df4631632ad353e5a37
Neville Oronni <nevooronni@gmail.com>
2018-03-23 22:22:58 +0300

    added responsive navbar that couldn't get to work in vue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+//component decorator for component configuration
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
+//this is where all the logic of your application go into e.g dependency injection 
 export class AppComponent {
-  title = 'app';
+
+	toggleNav: false;
 }
